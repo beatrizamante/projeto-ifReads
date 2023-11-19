764a3cc11e68af741c4567cd8f6a46400adb7c82
Beatriz Amante <beatrizamante@hotmail.com>
2023-11-19 20:26:52 -0300

    Change navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    borderRadius: {
+      none: "0",
+      sm: "0.125rem",
+      DEFAULT: "0.25rem",
+      DEFAULT: "4px",
+      md: "0.375rem",
+      lg: "1.5rem",
+      full: "9999px",
+      large: "12px",
+    },
     extend: {
       fontFamily: {
         serif: ["Comfortaa", "cursive"],
